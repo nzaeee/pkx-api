4cc42a07b91783721dde3ddc5321b9253272a9f4
nzxcx <hugo.laclau@gmail.com>
2025-06-15 19:20:11 +0200

    chore: restart

--- sql/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
-); 
